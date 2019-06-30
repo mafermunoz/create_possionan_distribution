c021e5127c2e3b8eadfebd0f7082ae9bed81ce4d
Maria Munoz <maria@Marias-MacBook-Pro-3.local>
2019-06-30 16:21:19 +0800

    Update poi_dist.py

--- poi_dist.py
+++ poi_dist.py
@@ -7,16 +7,16 @@ import glob
 
 def main(nmap,energy_range,year):
 
-nmap=int(nmap) ## nmap =1-100
-energy_range=int(energy_range)##1,2,3
-year=int(year)
+    nmap=int(nmap) ## nmap =1-100
+    energy_range=int(energy_range)##1,2,3
+    year=int(year)
 
-rate=np.load(' ../../ani_random_average/ani_avg_method/rate_ps_year_energybin.npy')
-total_sec=np.load(' ../../ani_random_average/ani_avg_method/total_seconds_peryear.npy')
+    rate=np.load(' ../../ani_random_average/ani_avg_method/rate_ps_year_energybin.npy')
+    total_sec=np.load(' ../../ani_random_average/ani_avg_method/total_seconds_peryear.npy')
 
-x=rate[year-1,energy_range-1]/total_sec[year-1]
-s = np.random.poisson(x,int(pointing_history[year-1]))
-np.save('../pois_dist_year_'+str(year)+"_energy_bin"+str(energy_range)+'_nmap_'+str(nmap)+'.npy')
+    x=rate[year-1,energy_range-1]/total_sec[year-1]
+    s = np.random.poisson(x,int(pointing_history[year-1]))
+    np.save('../pois_dist_year_'+str(year)+"_energy_bin"+str(energy_range)+'_nmap_'+str(nmap)+'.npy')
 
 if __name__ == '__main__':
         main(sys.argv[1],sys.argv[2],sys.argv[3])
