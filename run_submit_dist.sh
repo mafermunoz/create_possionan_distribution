c0d54aba0323612df6b12be6213caabb1115886b
Maria Munoz <maria@Marias-MacBook-Pro-3.local>
2019-07-10 18:49:52 +0800

    g

--- run_submit_dist.sh
+++ run_submit_dist.sh
@@ -2,9 +2,9 @@ for l in {1..100}
 do
   for k in {1..3}
   do
-    for i in {1..3}
+    for i in {1..1}
       do
-          OUTF='../pois_dist_year_'$i"_energy_bin"$k'_nmap_'$l'.npy'
+          OUTF='../pois_dist_year_new_'$i"_energy_bin"$k'_nmap_'$l'.npy'
           if [ ! -f ${OUTF} ]; then
             echo $l $k $i
             sbatch submit_dist.sh  $l $k $i
